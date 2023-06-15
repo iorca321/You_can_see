edcd8bd8363cb16f082fb1f087c632546631232b
Iorca <detailing.alarm.club@gmail.com>
2023-06-15 15:17:55 +0300

    path

--- VS_Lecture/gulpfile.js
+++ VS_Lecture/gulpfile.js
@@ -1,4 +1,16 @@
 "use strict"
 
-const {src, dest} = require("gulp")
-const gulp = require("gulp")
+const {src, dest} = require("gulp");
+const gulp = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const cssbeautify = require("gulp-cssbeautify");
+const sass = require("gulp-sass")(require("sass"));
+const cssnano = require("gulp-cssnano");
+const uglify = require("gulp-uglify");
+const plumber = require("gulp-plumber");
+const panini = require("panini");
+const imagemin = require("gulp-imagemin");
+const rename = require("gulp-rename");
+const rigger = require("gulp-rigger");
+const removeComments = require("gulp-strip-css-comments");
+const del = require("del");
